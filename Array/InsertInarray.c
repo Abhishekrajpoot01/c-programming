dcc697a86f69d8139e9bf7d3f2d0cad791501d1f
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-05 22:03:52 +0530

    element ask by user to insert

---
+++ Array/InsertInarray.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+  int i,n,num,pos,arr[10];
+  printf("\n enter the size of the element");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    printf("\n arr[%d]=",i);
+    scanf("%d",&arr[i]);
+  }
+  printf("enter the element to be added");
+  scanf("%d",&num);
+  printf("enter the position on which you want to add elemnt");
+}
